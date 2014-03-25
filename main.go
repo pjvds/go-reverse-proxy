ea6bc98513a3daa6e3e8af7cfcdaf2a3d4e32341
Pieter Joost van de Sande <pj@born2code.net>
2014-03-25 16:45:04 +0100

    Adds main.go that acts as a simple reverse proxy

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var (
+	base = flag.String("base", "", "Forward to, e.q.: http://myotherdomain.com/")
+	host = flag.String("host", "localhost:8080", "The host for the proxy")
+)
+
+func main() {
+	flag.Parse()
+
+	if base == nil || *base == "" {
+		fmt.Printf("you must specify the 'base' flag, re-run the program with: -base='http://mytargethost.com'\n")
+		return
+	}
+	if host == nil || *host == "" {
+		fmt.Printf("you must specify the 'host' flag, re-run the program with: -host=':80'\n")
+		return
+	}
+
+	baseUrl, err := url.Parse(*base)
+	if err != nil {
+		fmt.Printf("invalid target value %v, %v", *baseUrl, err)
+		return
+	}
+
+	fmt.Printf("hosting proxy at %v\n", *host)
+	proxy := httputil.NewSingleHostReverseProxy(baseUrl)
+	if err := http.ListenAndServe(*host, proxy); err != nil {
+		fmt.Printf("fatal: %v\n", err)
+	}
+}
